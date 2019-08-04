93043c38a3ff123d4eb4e04b39b409e87ab445a9
Tom White <eugene.white@gmail.com>
2019-08-04 16:07:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,23 @@
 class Student
 
-
+attr_accessor :name, :album, :id
+ 
+  def initialize(name, album, id=nil)
+    @id = id
+    @name = name
+    @album = album
+  end
+ 
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, album) 
+      VALUES (?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, self.name, self.album)
+ 
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+ 
+  end
   
 end
